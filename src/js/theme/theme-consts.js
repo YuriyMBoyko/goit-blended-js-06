8d1e40521048862e3561cdaaa0713d451370ae7d
Yuriy Boyko <YuriyMBoyko@gmail.com>
2026-08-03 10:03:39 +0300

    Theme is fixed.

---
+++ src/js/theme/theme-consts.js
@@ -0,0 +1,7 @@
+export const STORAGE_KEYS = {
+  THEME: 'blended-js6-theme',
+}
+
+export const THEME_ATTR_NAME = 'data-theme';
+export const LIGHT_THEME_ATTR_VALUE = 'light';
+export const DARK_THEME_ATTR_VALUE = 'dark';
--- src/js/theme/theme-logic.js
+++ src/js/theme/theme-logic.js
@@ -1,16 +1,18 @@
 import refs from './theme-refs.js';
-import theme_init from './theme-init.js?raw';
+//import theme_init from './theme-init.js?raw';
+import { STORAGE_KEYS, THEME_ATTR_NAME, LIGHT_THEME_ATTR_VALUE, DARK_THEME_ATTR_VALUE } from './theme-consts.js';
 
 refs.taggleButton?.addEventListener('click', () => {
-  const newTheme = (isCurrentThemeDark() ? LIGHT_THEME_ATTR_VALUE : DARK_THEME_ATTR_VALUE);
+  const newTheme = (isCurrentThemeDark() ? String(LIGHT_THEME_ATTR_VALUE) : String(DARK_THEME_ATTR_VALUE));
+  console.log(newTheme);
   setTheme(newTheme);
   saveTheme(newTheme);
 });
 
 function currentTheme() {
-  return (document.documentElement.hasAttribute(THEME_ATTR_NAME) && (document.documentElement.getAttribute(THEME_ATTR_NAME).toLowerCase() === DARK_THEME_ATTR_VALUE.toLowerCase())) 
+  return (document.documentElement.hasAttribute(THEME_ATTR_NAME) && (document.documentElement.getAttribute(THEME_ATTR_NAME).toLowerCase() === String(DARK_THEME_ATTR_VALUE).toLowerCase())) 
     ? DARK_THEME_ATTR_VALUE
-    : ((document.body.hasAttribute(THEME_ATTR_NAME) && (document.body.getAttribute(THEME_ATTR_NAME).toLowerCase() === DARK_THEME_ATTR_VALUE.toLowerCase())) ? DARK_THEME_ATTR_VALUE : LIGHT_THEME_ATTR_VALUE);
+    : ((document.body.hasAttribute(THEME_ATTR_NAME) && (document.body.getAttribute(THEME_ATTR_NAME).toLowerCase() === String(DARK_THEME_ATTR_VALUE).toLowerCase())) ? DARK_THEME_ATTR_VALUE : LIGHT_THEME_ATTR_VALUE);
 }
 
 export function isCurrentThemeDark() {
@@ -22,14 +24,14 @@ export function isCurrentThemeLight() {
 }
 
 function isDarkTheme(theme) {
-  return String(theme).toLowerCase() === DARK_THEME_ATTR_VALUE.toLowerCase();
+  return String(theme).toLowerCase() === String(DARK_THEME_ATTR_VALUE).toLowerCase();
 }
 
 function saveTheme(theme) {
   if (isDarkTheme(theme)) {
-    localStorage.setItem(STORAGE_KEYS.THEME, String(theme).toLowerCase());
+    localStorage.setItem(Object(STORAGE_KEYS).THEME, String(theme).toLowerCase());
   } else {
-    localStorage.removeItem(STORAGE_KEYS.THEME);
+    localStorage.removeItem(Object(STORAGE_KEYS).THEME);
   }
 }
 
